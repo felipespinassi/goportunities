e59474e1aca089bb59754e0d41707af1874e7a9a
Felipe Augusto <felipe_spinassi@hotmail.com>
2023-11-11 13:28:27 -0300

    feat: add first route

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-	print("Hello world")
+
+	//inicialza o router usando configuraçoes default do gin
+	r := gin.Default()
+	//definindo uma rota
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
