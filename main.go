1d3e7bf9eb5a00c52e9cf98e2c80456c9c6e9fd9
Felipe Augusto <felipe_spinassi@hotmail.com>
2024-05-17 15:38:23 -0300

    feat: add log and gorm

--- main.go
+++ main.go
@@ -1,8 +1,20 @@
 package main
 
-import "github.com/felipespinassi/goportunities/router"
+import (
+	"fmt"
+
+	"github.com/felipespinassi/goportunities/config"
+	"github.com/felipespinassi/goportunities/router"
+)
 
 func main() {
+	//initialize configs
+	err := config.Init()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 	//initialize router
 	router.Initialize()
 }
